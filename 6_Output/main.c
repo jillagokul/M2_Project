7bfb97116d240d997a1af31431461d7c28f64ef2
Gokul <101977970+jillagokul@users.noreply.github.com>
2022-04-22 10:20:58 +0530

    Add files via upload

---
+++ 6_Output/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 4/20/2022 2:30:05 PM
+ *  Author: Welcome
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
